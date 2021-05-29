587a59a52108844952a843b31b127301cb8fc981
JACPro <jamesacpro@gmail.com>
2021-05-29 09:04:36 +0100

    Add iphone model

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] GameObject objectPrefab;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
