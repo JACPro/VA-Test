36a41425f348a56fb969dbc057b48c687da78718
JACPro <jamesacpro@gmail.com>
2021-05-29 16:13:25 +0100

    Add explode an unexplode animations

---
+++ Assets/AnimationManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animation;
+
+
+public class AnimationManager : MonoBehaviour
+{
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
